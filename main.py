69d4d38d8c31e9062995fa6a39a53756f412b385
Devin Zhou <devinzhou101@gmail.com>
2021-05-15 23:21:22 -0400

    linearregression

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from functools import reduce
+from sklearn.linear_model import LinearRegression
 
 
 newcases = pd.read_csv("csv/newCases.csv")
@@ -20,7 +21,7 @@ df = reduce(lambda x, y: pd.merge(
 
 
 df.rename(columns={"category": "Date",
-          "Active (including people who are hospitalized)": "Active", "Hospitalized (including people in ICU)": "Hospitalized", "Positive results": "Positive result (%)", "Total tests completed": "Total tests done", "previous_day_doses_administered": "Previous day doses adminstered", "total_doses_administered": "Total doses adminstered", "total_individuals_fully_vaccinated": "Total fully vaccinated"}, inplace=True)
+          "Active (including people who are hospitalized)": "Active cases", "Hospitalized (including people in ICU)": "Hospitalized", "Positive results": "Positive result (%)", "Total tests completed": "Total tests done", "previous_day_doses_administered": "Previous day doses adminstered", "total_doses_administered": "Total doses adminstered", "total_individuals_fully_vaccinated": "Total fully vaccinated"}, inplace=True)
 df.drop(columns=["Critical threshold",
         "total_doses_in_fully_vaccinated_individuals"], inplace=True)
 
@@ -30,9 +31,43 @@ df.drop(index=462, inplace=True)
 # Removes 1 from Total cases
 df["Total cases"] = df["Total cases"] - 1
 
-print(df)
+# fill empty values with 0
+df = df.fillna(0)
 
-df = df.to_excel("df.xlsx")
+# print(df)
+
+df.to_excel("df.xlsx")
+
+# print(df.describe())
+
+len = int(len(df) * 0.8)
+# print(len)
+# print(len)
+
+training = df.iloc[:len]
+trainingx = training["New cases"].values.reshape(-1, 1)
+# print(trainingx)
+
+trainingy = training["New deaths"].values.reshape(-1, 1)
+# print(trainingy)
+
+
+ml = LinearRegression()
+ml.fit(trainingx, trainingy)
+predy = ml.predict(trainingx)
+# print(predy)
+
+plt.xlabel("New cases")
+plt.ylabel("New Deaths")
+# plt.figure(figsize=(15, 7))
+plt.scatter(trainingx, trainingy)
+plt.plot(trainingx, predy)
+plt.show()
+
+
+# predicty = ml.predict(validationx)
+# print(validationy)
+# print(predicty)
 
 
 # linear regression to predict future values
